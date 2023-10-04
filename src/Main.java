784edffbeca3b937fe24a24fc595c50ae64d2bde
sergei <sergei13121390@gmail.com>
2023-10-04 22:48:33 +0300

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog= " + dog);
+        System.out.println("cat= " + cat);
+        System.out.println("paper= " + paper);
+
+    }
+}
